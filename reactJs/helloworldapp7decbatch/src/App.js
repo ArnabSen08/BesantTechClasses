d6980e553143c134e52e254de0a3fffb1d5a76da
ArnabSen08 <beanclarksum@gmail.com>
2022-12-08 21:12:49 +0530

    08-12-2022

--- reactJs/helloworldapp7decbatch/src/App.js
+++ reactJs/helloworldapp7decbatch/src/App.js
@@ -4,9 +4,16 @@ import './App.css';
 import MyFirstComp from './MyFirstComp';
 
 function App() {
+  var Details = {
+    Name : "Arnab",
+    Age : 32,
+    Desg : "Analyst"
+  };
+
+  var CountriesName = ["UK","USA","India"];
   return (
     <div>
-      <MyFirstComp />
+      <MyFirstComp UserDetails = {Details} Countries = {CountriesName}/>
 
     </div>
   );
--- reactJs/helloworldapp7decbatch/src/MyFirstComp.js
+++ reactJs/helloworldapp7decbatch/src/MyFirstComp.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 export default class MyFirstComp extends React.Component{
     render(){
+        console.log(this.props);
         return <div>
             Hello World;
             <br />
             <button>Click ME</button>
+            {this.props.UserDetails.Name};
+            <br />
+            {this.props.Countries};
         </div>
     }
 }
