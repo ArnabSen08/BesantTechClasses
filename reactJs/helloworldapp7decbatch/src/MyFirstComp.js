bbf10f8fc063844a04ec05f00bf03eb179339261
ArnabSen08 <beanclarksum@gmail.com>
2022-12-09 20:41:40 +0530

    09-12-2022

--- reactJs/helloworldapp7decbatch/src/MyFirstComp.js
+++ reactJs/helloworldapp7decbatch/src/MyFirstComp.js
@@ -1,6 +1,27 @@
 import React from 'react';
 
 export default class MyFirstComp extends React.Component{
+    constructor(props){
+        super(props);
+        //this.Name="Arnab";
+        this.State={Name: "Arnab Sen",
+        Age:32}
+    }
+
+    handleChange=(e)=>{
+        this.setState({
+            [e.target.Name] : e.target.value
+            
+        })
+    };
+
+    handleChange1(e){
+        this.setState({
+            [e.target.Name] : e.target.value
+            
+        })
+    }
+
     render(){
         console.log(this.props);
         return <div>
@@ -10,6 +31,17 @@ export default class MyFirstComp extends React.Component{
             {this.props.UserDetails.Name};
             <br />
             {this.props.Countries};
+            {this.State.Name};
+            <br/>
+            <input type="text" name="Name" onChange={this.handleChange} />
+            <br/>
+            {this.State.Age};
+            <br/>
+            <input type="text" name="Age" onChange={this.handleChange1} />
+            <br/>
         </div>
     }
 }
+
+//Normal Function has local "this" ?? "this" inside function it refers function env
+//Arrow function always refers to global "this"
